79ec8ae380cd07ba33d32b09ec232e7d867e6dfb
HardCoderNET <hcn@101.0.168.192.in-addr.arpa>
2017-10-24 07:59:09 +0300

    fix mac os app

--- dev/sources/zuidhorn-electron-app/js/app.js
+++ dev/sources/zuidhorn-electron-app/js/app.js
@@ -9,50 +9,51 @@ if (typeof require != 'undefined') {
         }
     });
 
-    dataStorage = new(function() {
-        var fs = require('fs');
-        var rimraf = require('rimraf');
-        var path = './storage';
-
-        if (!fs.existsSync(path))
-            fs.mkdirSync(path);
-
-        var makePath = function(itemName) {
-            return path + '/' + itemName + '.data';
-        };
-
-        this.getItem = function(itemName) {
-            var filePath = makePath(itemName);
-
-            if (fs.existsSync(filePath))
-                return fs.readFileSync(filePath);
-
-            return null;
-        };
-
-        this.setItem = function(itemName, content) {
-            var filePath = makePath(itemName);
-
-            this.removeItem(itemName);
-
-            return fs.writeFileSync(filePath, content);
-        };
-
-        this.removeItem = function(itemName, content) {
-            var filePath = makePath(itemName);
-
-            if (fs.existsSync(filePath))
-                return fs.unlinkSync(filePath);
-
-            return null;
-        };
-
-        this.clear = function() {
-            rimraf.sync(path + '/*.data');
-        };
-    })();
-
     // use native localStorage on mac os
-    if (require('os').platform() == "darwin")
+    if (require('os').platform() == "darwin") {
         dataStorage = localStorage;
+    } else {
+        dataStorage = new(function() {
+            var fs = require('fs');
+            var rimraf = require('rimraf');
+            var path = './storage';
+
+            if (!fs.existsSync(path))
+                fs.mkdirSync(path);
+
+            var makePath = function(itemName) {
+                return path + '/' + itemName + '.data';
+            };
+
+            this.getItem = function(itemName) {
+                var filePath = makePath(itemName);
+
+                if (fs.existsSync(filePath))
+                    return fs.readFileSync(filePath);
+
+                return null;
+            };
+
+            this.setItem = function(itemName, content) {
+                var filePath = makePath(itemName);
+
+                this.removeItem(itemName);
+
+                return fs.writeFileSync(filePath, content);
+            };
+
+            this.removeItem = function(itemName, content) {
+                var filePath = makePath(itemName);
+
+                if (fs.existsSync(filePath))
+                    return fs.unlinkSync(filePath);
+
+                return null;
+            };
+
+            this.clear = function() {
+                rimraf.sync(path + '/*.data');
+            };
+        })();
+    }
 }
